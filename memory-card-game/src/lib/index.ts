3c92f546ca5f984e81d32756c22507441deeb164
A.E. Özbay <ozbay.ae@gmail.com>
2024-10-31 18:42:05 +0100

    remove everything

--- memory-card-game/src/lib/index.ts
+++
@@ -1,8 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-if (import.meta.vitest) {
-	const { it, expect } = import.meta.vitest;
-	it('should have a test to supress failed test file'),
-		() => {
-			expect(true).toBe(true);
-		};
-}
--- memory-card-game/src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	import Game from '../Game.svelte';
-</script>
-
-<Game />
